e1e3749ecbffcaee578d4d0a369c79b2bb63ac78
David Hearn <davidhearn90@gmail.com>
2015-05-20 09:19:49 -0700

    started page

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+	<title>David Hearn</title>
+</head>
+<header>
+  <nav id="nav-1">
+  <a class="link-1" href="http://localhost/landingpage/index.php">Home</a>
+  <a class="link-1" href="http://localhost/landingpage/about.php">About</a>
+  <a class="link-1" href="#">Contact</a>
+</nav>
+
+
+</header>
+
+<body>
+
+<div id="container">
+<div id="text">
+  <span>D</span>
+  <span>A</span>
+  <span>V</span>
+  <span>I</span>
+  <span>D</span>
+</div>
+
+<div id="text">
+  <span>H</span>
+  <span>E</span>
+  <span>A</span>
+  <span>R</span>
+  <span>N</span>
+</div>
+
+</body>
+<script type="text/javascript" src="js/main.js"></script>
+</html>
